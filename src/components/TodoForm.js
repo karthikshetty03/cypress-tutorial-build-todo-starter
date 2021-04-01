fb1c897a512c983e835792ade3bde98dc4e65851
Karthik Shetty <f20180141@hyderabad.bits-pilani.ac.in>
2021-04-01 10:11:12 +0530

    First set of test

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -5,5 +5,8 @@ export default props =>
     <input
       type='text'
       className="new-todo"
+      autoFocus
+      value = {props.currentTodo}
+      onChange = {props.handleNewTodoChange}
       placeholder="What needs to be done?"/>
   </form>
