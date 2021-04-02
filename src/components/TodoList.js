39414bcba81c2e0580311936fc8f1edddceb25f4
Karthik Shetty <f20180141@hyderabad.bits-pilani.ac.in>
2021-04-03 01:14:34 +0530

    Debugging

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,12 @@ import React from "react";
 const TodoItem = (props) => (
   <li className={props.isComplete ? "completed" : null}>
     <div className="view">
-      <input className="toggle" type="checkbox" checked={props.isComplete} />
+      <input
+        className="toggle"
+        type="checkbox"
+        checked={props.isComplete}
+        onChange={() => props.handleTodoUpdate(props.id)}
+      />
       <label>{props.name}</label>
       <button
         className="destroy"
@@ -17,7 +22,12 @@ export default (props) => (
   <div>
     <ul className="todo-list">
       {props.todos.map((todo) => (
-        <TodoItem handleDelete={props.handleDelete} key={todo.id} {...todo} />
+        <TodoItem
+          handleTodoUpdate={props.handleTodoUpdate}
+          handleDelete={props.handleDelete}
+          key={todo.id}
+          {...todo}
+        />
       ))}
     </ul>
   </div>
